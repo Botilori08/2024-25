9d7a220f7c7f809531f2037f3599bd388c69c98d
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-09-20 13:01:45 +0200

    09.20

---
+++ Aaf C#/Homework/Hf 09.19/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Hf_09._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. feladat");
+            Console.WriteLine("Hárommal osztható háromjegyű számok:");
+
+            Console.WriteLine("1. megoldás:");
+            for (int i = 102; i <=999;i++)
+            {
+                if (i %3 ==0)
+                {
+                    Console.Write(i+";");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("2. megoldás:");
+            for (int i = 102;i<1000;i+=3)
+            {
+                Console.Write(i+";");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("2. feladat");
+            for (char betu = 'A';betu <= 'Z';betu++)
+            {
+                Console.Write(betu + ";");
+            }
+
+        }
+    }
+}
