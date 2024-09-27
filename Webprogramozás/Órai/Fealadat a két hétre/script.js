0de756a54886860611078ef1e78321da5b8c6eb8
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-09-27 09:38:06 +0200

    09.27

--- Webprogramozás/Órai/Fealadat a két hétre/script.js
+++ Webprogramozás/Órai/Fealadat a két hétre/script.js
@@ -24,5 +24,41 @@ function negyzetreEmel(){
         console.log(szamok[egySzam]*szamok[egySzam])
         negyzetek.push(szamok[egySzam]*szamok[egySzam])
     }
-    document.getElementById("negyzete").innerHTML = "A megadott számok: "+negyzetek
+    document.getElementById("negyzete").innerHTML = "A megadott számok négyzetei: "+negyzetek
+}
+
+
+function Atvalto(){
+    var centimeter = prompt("Kérek egy centiméter értéket! ")
+    var meter = centimeter/100
+
+    document.getElementById("centimeterV").innerHTML = "A megadott érték: "+centimeter+" cm"
+    document.getElementById("atvaltas").innerHTML = "Átváltás: "+meter+" m"
+
+}
+
+function Masodfoku(){
+    var a = prompt("Mennyi legyen 'a' értéke?")
+    var b = prompt("Mennyi legyen 'b' értéke?")
+    var c = prompt("Mennyi legyen 'c' értéke?")
+
+    var szamlalox1 = b*-1 + Math.sqrt(((b*-1)**2)-(4*a*c))
+    var szamlalox2 = b*-1 - Math.sqrt(((b*-1)**2)-(4*a*c))
+
+    var nevezo = a*2
+
+    var x1 = szamlalox1/nevezo
+    var x2 = szamlalox2/nevezo
+
+    document.getElementById("szamok").innerHTML = "a= "+a+"; "+"b= "+b+"; "+"c= "+c+" "
+
+    console.log(x1)
+    console.log(x2)
+
+    document.getElementById("megoldas1").innerHTML = "x1= "+ x1
+    document.getElementById("megoldas2").innerHTML = "x2= "+ x2
+
+
+
+
 }
