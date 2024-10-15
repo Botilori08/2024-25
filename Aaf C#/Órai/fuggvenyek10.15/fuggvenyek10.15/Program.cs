f13cd7d296c163d887a00ad957c03505a96b3559
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-10-15 08:47:18 +0200

    10.15

---
+++ Aaf C#/Órai/fuggvenyek10.15/fuggvenyek10.15/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace fuggvenyek10._15
+{
+    internal class Program
+    {
+
+        static int osszead(int elsoSzam, int masodikSzam)
+        {
+            return elsoSzam + masodikSzam;
+        }
+
+        static double osszead(double elsoSzam, double masodikSzam)
+        {
+            return elsoSzam + masodikSzam;
+        }
+
+        static double oszt(int elsoSzam, int masodikSzam)
+        {
+            
+            return (double)elsoSzam / (double)masodikSzam;
+        }
+        static double oszt(double elsoSzam, double masodikSzam)
+        {
+
+            return elsoSzam / masodikSzam;
+        }
+
+        // A paraméterként kapott egész számokat összeadja. Kiírja a végredményt.
+        static void szumma(int[] szamok)
+        {
+            int osszeg = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                osszeg += szamok[i];
+            }
+            Console.WriteLine(osszeg);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Kérem az első számot!: ");
+            //int firstSzam = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Kérem a második számot!")
+
+            Console.WriteLine(osszead(4, 4));
+            Console.WriteLine(osszead(-1, 7));
+            Console.WriteLine(osszead(2,3.5));
+            Console.WriteLine();
+            Console.WriteLine(oszt(2,1));
+            Console.WriteLine(oszt(2, 2));
+            Console.WriteLine(oszt(2, 4));
+            Console.WriteLine(oszt(2.5, 1.2));
+
+            szumma(2,2,2,2,2);
+        }
+    }
+}
