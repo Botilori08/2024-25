289ce5b4c95c921a56901d829ec30741cc3a5335
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-10-25 08:46:02 +0200

    10.25

--- Aaf C#/Órai/gyakorlas10.24/Program.cs
+++ Aaf C#/Órai/gyakorlas10.24/Program.cs
@@ -1,48 +1,76 @@
 ﻿namespace gyakorlas10._24
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Random rand = new Random();
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			
+			
+			Random rand = new Random();
+			
+			int kitalalt = rand.Next(0, 10);
+			Console.WriteLine("Számkitalálós 1. változat");
 
-            int kitalalt = rand.Next(0, 10);
-            Console.WriteLine("Gondoltam egy számra. Melyik lehet az?");
+			Console.WriteLine("Gondoltam egy számra. Melyik lehet az?");
+
+			int tipp = 0;
+
+			while (tipp != kitalalt)
+			{
+				Console.Write("Tippelj: ");
+				tipp = Convert.ToInt32(Console.ReadLine());
+				if (tipp < kitalalt)
+				{
+					Console.WriteLine("Túl kicsi. A számom nagyobb");
+				}
+				else if (tipp > kitalalt)
+				{
+					Console.WriteLine("Túl nagy! A gondolt szám kisebb");
+				}
+				else
+				{
+					Console.WriteLine("Eltaláltad!");
+					break;
+				}
+			}
 
 
 
-            int tipp = 0;
+            Console.WriteLine();
+            Console.WriteLine("2. változat");
 
-            while (tipp != kitalalt)
-            {
-                Console.Write("Tippelj: ");
-                tipp = Convert.ToInt32(Console.ReadLine());
+            int gondolt = 6;
+            int gepTipp = rand.Next(0, 10);
 
-                if (tipp < kitalalt)
-                {
-                    Console.WriteLine("Túl kicsi. A számom nagyobb");
-                }
-                else if (tipp > kitalalt)
-                {
-                    Console.WriteLine("Túl nagy! A gondolt szám kisebb");
-                }
-                else
-                {
-                    Console.WriteLine("Eltaláltad!");
-                    break;
-                }
-            }
+            while (gepTipp != gondolt)
+			{
+				if (gepTipp < gondolt)
+				{
+					Console.WriteLine(gepTipp);
+					Console.Write()
+					gepTipp = rand.Next(0, 10);
+				}
 
 
 
-
-
-            //Számkitalálós játék
-            // 1. Változat: Gép gondol egy számra, találgatunk
-            // Gép ad tippet 
-            // 2.: felhasználó gondol egy számra és a gép találgat
+			}
 
 
         }
-    }
+
+
+			
+
+
+
+
+			//Számkitalálós játék
+			// 1. Változat: Gép gondol egy számra, találgatunk
+			// Gép ad tippet 
+			// 2.: felhasználó gondol egy számra és a gép találgat
+
+
+		}
+	}
 }
