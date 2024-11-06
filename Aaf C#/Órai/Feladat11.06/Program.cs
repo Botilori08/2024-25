e9a5574015d2c0e9b5ed1f034eab6322f3fcf508
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-11-06 14:20:25 +0100

    11.06

---
+++ Aaf C#/Órai/Feladat11.06/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace Feladat11._06
+{
+    internal class Program
+    {
+        static Random rand = new Random();
+        static int[] Randomszam()
+        {
+            Console.Write("Mettől sorsoljak számokat? ");
+            int alsoH = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Meddig sorsoljak számokat? ");
+            int felsoH = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hány számot sorsoljak? ");
+            int darab = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            int[] szamok = new int[darab];
+
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                int szam = rand.Next(alsoH, felsoH);
+                szamok[i] = szam;
+            }
+            return szamok;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] Alista = Randomszam();
+            for (int i = 0;i < Alista.Length; i++)
+            {
+                Console.Write(Alista[i]+" ");
+            }
+        
+        }
+
+    }
+}
