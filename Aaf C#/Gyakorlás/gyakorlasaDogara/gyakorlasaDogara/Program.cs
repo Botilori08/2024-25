d52371a046c59b1c7eb62954cc254a7274a729f1
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-11-11 10:35:30 +0100

    11.11

---
+++ Aaf C#/Gyakorlás/gyakorlasaDogara/gyakorlasaDogara/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace gyakorlasaDogara
+{
+    internal class Program
+    {
+
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            //Kérjünk be egy számot, mennyi legyen a lista elemszáma, ami két random szám között generál random számot, ezt függvénnyel.
+
+            int szam = rand.Next(rand.Next(20, 30), rand.Next(700, 1000));
+
+            Console.WriteLine(szam);
+        }
+    }
+}
