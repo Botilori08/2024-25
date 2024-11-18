00c6054ec1389fb9e07395ddf29f0302ece6b458
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-11-18 13:30:22 +0100

    11.18

---
+++ Aaf C#/Órai/filekezeles11.18/filekezeles11.18/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace filekezeles11._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter ir = new StreamWriter("proba.txt");
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                ir.Write(rand.Next(10,100));
+                ir.Write("\t");
+
+                if (i%10 == 9)
+                {
+                    ir.WriteLine();
+                }
+            }
+
+            ir.Close();
+        }
+    }
+}
