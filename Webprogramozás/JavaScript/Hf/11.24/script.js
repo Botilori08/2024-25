f5fbbd13549beaa47da31a20ee2c26243ec20e35
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2024-11-26 18:47:10 +0100

    11.24 Hf

---
+++ Webprogramozás/JavaScript/Hf/11.24/script.js
@@ -0,0 +1,21 @@
+
+
+
+const szoveg = "Szióka";
+console.log(typeof szoveg);
+
+const szam = 2.34;
+console.log(typeof szam);
+
+const igaz = true;
+console.log(typeof igaz);
+
+const tomb = ["Lajos","Géza","Judit", "Pista"];
+console.log(typeof tomb);
+
+const teljesNev = {vezetekN:"Horváth",keresztN:"Lajos"};
+console.log(typeof teljesNev);
+
+
+
+
