dad3eb381b3b6e945d487c2d9477ac012cbe1b6f
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2024-11-28 16:25:27 +0100

    11.28

---
+++ Aaf C#/XP feladatok/szit 0801/dolgozok/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace dolgozok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] adatok = File.ReadAllLines("dolgozok.txt");
+
+            int[] penzek = new int[adatok.Length];
+
+
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] vago = adatok[i].Split(":");
+                penzek[i] = Convert.ToInt32(vago[4]);                
+            }
+
+            int osszeg = 0;
+
+            for (int i = 0;i < penzek.Length;i++)
+            {
+                osszeg += penzek[i];
+            }
+
+            Console.WriteLine($"A dolgozók fizetéseinek összege: {osszeg} Ft");
+
+
+        }
+    }
+}
