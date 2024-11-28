756958aaf1792f2cdcee63522e511ce129e26881
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-11-28 14:08:22 +0100

    11.28

---
+++ Aaf C#/file kezeles dolgozat/file kezeles dolgozat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace file_kezeles_dolgozat
+{
+	internal class Program
+	{
+		static StreamReader reader = new StreamReader("kod_B.txt");
+		static void Main(string[] args)
+		{
+			string kod = reader.ReadLine();
+
+			Console.WriteLine(kod);
+
+			string kodok = reader.ReadToEnd();
+
+			string[] tomb = kodok.Split("\n");
+
+			int[] ujtomb = new int[tomb.Length];
+			for (int i = 0; i < tomb.Length; i++)
+			{
+				for (int j = 0; j < ujtomb.Length; j++)
+				{
+					ujtomb[j] = int.Parse(tomb[i]);
+				}
+				
+			}
+
+			for (int i = 0; i < ujtomb.Length; i++)
+			{
+				Console.WriteLine(ujtomb[i]);
+			}
+
+			
+		}
+	}
+}
