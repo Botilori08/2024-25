1050fcaed16411622a018c5eed862c76defa9d54
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-12-03 08:47:03 +0100

    12.03

---
+++ Aaf C#/Órai/Dictionary12.03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Dictionary12._03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine();
+			Dictionary<string, string> dict = new Dictionary<string, string>();
+
+			dict.Add("Ádám", "Fortnite");
+			dict.Add("Zolika", "Wrum-wrum");
+			dict.Add("Bazsi", "PHP SQL Király");
+
+			Console.WriteLine(dict["Ádám"]);
+            
+
+        }
+	}
+}
