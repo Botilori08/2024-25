413636b918f0ab514096ceb70d0745546b08ec91
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-12-06 13:31:25 +0100

    12.06

---
+++ Aaf C#/Órai/12.06/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _12._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            macska m = new macska("Cirmi");
+
+            Console.WriteLine(m.nev);
+
+            //Hf valamilyen osztályt megalkotni
+        }
+    }
+}
