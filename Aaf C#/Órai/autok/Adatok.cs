b6f34c55648f4211d9e7c94e36695b02b3070f4a
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2025-01-07 11:30:30 +0100

    01.07

---
+++ Aaf C#/Órai/autok/Adatok.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok_01._07
+{
+    internal class Adatok
+    {
+        public string rendszam;
+        public int ora;
+        public int perc;
+        public int sebesseg;
+
+        public Adatok(string rendszam, int ora, int perc,int sebesseg)
+        {
+            this.rendszam = rendszam;
+            this.ora = ora;
+            this.perc = perc;
+            this.sebesseg = sebesseg;
+        }
+
+
+        public Adatok(string sor)
+        {
+            string[] vag = sor.Split("\t");
+
+            rendszam = vag[0];
+            ora = Convert.ToInt32(vag[1]);
+            perc = Convert.ToInt32(vag[2]);
+            sebesseg = Convert.ToInt32(vag[3]);
+        }
+    }
+}
