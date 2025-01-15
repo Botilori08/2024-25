8814741ffd63982a45d902ae79ca6b59076cd080
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2025-01-15 14:20:38 +0100

    01.15

--- Aaf C#/Órai/robot/Adatok.cs
+++ Aaf C#/Órai/robot/Adatok.cs
@@ -17,5 +17,35 @@ namespace robot
             nev = vag[0];
             kod = vag[1];
         }
+
+        public bool hibas()
+        {
+            return kod.Replace("E", "").Replace("H", "").Replace("J", "").Replace("B", "").Length !=0;
+            bool valasz = false;
+            for (int i = 0; i < kod.Length; i++)
+            {
+                if(kod[i] != 'E' && kod[i] != 'H' && kod[i] != 'B' && kod[i] != 'J')
+                {
+                    valasz = true;
+                    break;
+                }
+            }
+
+            return valasz;
+            
+        }
+        public int iranyvaltasok()
+        {
+
+            int darab = 0;
+            for (int i = 1; i < kod.Length; i++)
+            {
+                if (kod[i] != kod[i - 1])
+                {
+                    darab++;
+                }
+            }
+            return darab;
+        }
     }
 }
--- Aaf C#/Órai/robot/Program.cs
+++ Aaf C#/Órai/robot/Program.cs
@@ -14,6 +14,32 @@
 			}
 
             Console.WriteLine($"2. feladat: Tanulók száma: {programok.Count} fő");
+
+            
+            int szamlalo = 0;
+			for (int i = 0; i < programok.Count; i++)
+			{
+				if (programok[i].hibas())
+				{
+					szamlalo++;
+				}
+			}
+
+            Console.WriteLine($"3. feladat: Helytelen kódsorozatok száma: {szamlalo}");
+
+			StreamWriter ir = new StreamWriter("ivsz.txt");
+
+			for (int i = 0; i < programok.Count; i++)
+			{
+				if (!programok[i].hibas())
+				{
+					ir.Write(programok[i].nev);
+					ir.WriteLine(programok[i].iranyvaltasok());
+				}
+
+			}
+
+			ir.Close();
         }
-	}
+    }
 }
