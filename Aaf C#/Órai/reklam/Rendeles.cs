6dfe11caa44ff4ddb7d89b5be439a30f70e02cb8
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2025-02-07 09:42:21 +0100

    02.07

---
+++ Aaf C#/Órai/reklam/Rendeles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reklam
+{
+    internal class Rendeles
+    {
+        public int nap;
+        public string varos;
+        public int darab;
+
+        public Rendeles(string sor)
+        {
+            string[] vag = sor.Split(" ");
+            nap = Convert.ToInt32(vag[0]);
+            varos = vag[1];
+            darab = Convert.ToInt32(vag[2]);
+        }
+    }
+}
