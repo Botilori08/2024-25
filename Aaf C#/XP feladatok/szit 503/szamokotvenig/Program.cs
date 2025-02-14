1d3b1f3fef578323a6c3331c08e95e9f1b44d43f
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-02-14 18:13:47 +0100

    02.14

---
+++ Aaf C#/XP feladatok/szit 503/szamokotvenig/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace szamokotvenig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Első módszer:");
+
+            for (int i = 0; i < 51; i++)
+            {
+                Console.Write(i+";");
+                if (i == 50)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Második módszer:");
+            int szam = 0;
+            while (szam < 50)
+            {
+                szam++;
+                Console.Write(szam+";");
+
+                if(szam == 50)
+                { 
+                    Console.Write(szam);
+                }
+            }
+
+        }
+    }
+}
