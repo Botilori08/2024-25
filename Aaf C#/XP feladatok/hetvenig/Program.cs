958c8a697f9108a06260bc42b97c5b6f4087e1fd
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2025-03-04 10:53:57 +0100

    03.04.

---
+++ Aaf C#/XP feladatok/hetvenig/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace hetvenig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boti Lóránt, 2025");
+            for (int i = 20; i < 70; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i+"-");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
