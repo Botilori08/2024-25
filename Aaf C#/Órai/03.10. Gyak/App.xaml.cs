45b6fd1566c31840da9ef7293b01fe15c31dcde5
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2025-03-10 13:16:40 +0100

    03.10.

---
+++ Aaf C#/Órai/03.10. Gyak/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _03._10._Gyak
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Aaf C#/Órai/03.10. Gyak/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03._10._Gyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> list = new List<string>();
+
+            list.Add("Pá rizs!");
+            list.Add("London-don-don-don");
+            list.Add("AmstelDayum!");
+            list.Add("Kappanhágó");
+
+            valaszt.ItemsSource = list;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            szamHelye.Text = csuszka.Value.ToString();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                csuszka.Value = Convert.ToDouble(szamHelye.Text);
+            }
+            catch
+            {
+
+            }
+        }
+
+
+    }
+}
