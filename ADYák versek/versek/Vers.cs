a9cc3aadbcdf9499a775eb1f3035ba4755c96a4c
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-06-02 18:02:26 +0200

    06.02

---
+++ ADYák versek/versek/Vers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace versek
+{
+    internal class Vers
+    {
+        public string cim;
+        public string versSzerzo;
+        public string versCim;
+        public string datum;
+
+        public Vers(string cim, string versSzerzo, string versCim, string datum)
+        {
+            this.cim = cim;
+            this.versSzerzo = versSzerzo;
+            this.versCim = versCim;
+            this.datum = datum;
+        }
+    }
+}
