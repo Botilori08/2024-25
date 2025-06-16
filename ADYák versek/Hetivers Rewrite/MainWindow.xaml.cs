cf67377b89521f64a4c060428d03817b571fddfb
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-06-16 09:20:26 +0200

    06.16

--- ADYák versek/Hetivers Rewrite/MainWindow.xaml.cs
+++ ADYák versek/Hetivers Rewrite/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,7 +35,6 @@ namespace versek
 		{
 
 
-
 			string[] hetiVersek = File.ReadAllLines("eddigiversek.txt");
 
 			for (int i = 0; i < hetiVersek.Length; i++)
@@ -47,16 +47,11 @@ namespace versek
 			for (int i = 0; i < versek.Count; i++)
 			{
 				versKiir.Add($"{versek[i].felelos} ; {versek[i].versSzerzo} - {versek[i].versCim} ; {versek[i].datum}");
+				MessageBox.Show(versek[i].URL());
 			}
 
 		}
-
-
-
-
-
-
-	private void kuldes_Click(object sender, RoutedEventArgs e)
+		private void kuldes_Click(object sender, RoutedEventArgs e)
 		{
 
 			string szerzo = szerzoNeve.Text;
@@ -64,60 +59,124 @@ namespace versek
 			string datum = Datum.Text;
 			string neve = nevKivalaszt.Text;
 
-			
 
-			StreamWriter sr = new StreamWriter("eddigiversek.txt",true,Encoding.UTF8);
 
-			if(szerzo !="" && versCime !="" && datum != "" && neve != "")
+			StreamWriter sr = new StreamWriter("eddigiversek.txt", true, Encoding.UTF8);
+
+			if (szerzo != "" && versCime != "" && datum != "" && neve != "")
 			{
-				versek.Add(new Vers(szerzo, versCime, neve, datum));
+				versek.Add(new Vers(neve, szerzo, versCime, datum));
 
 				sr.WriteLine($"{neve};{szerzo};{versCime};{datum}");
 
-				versKiir.Add($"{neve} ; {szerzo} - {versCime} ; {datum}");
+				versKiir.Add(versek[versek.Count-1].osszefuz());
 			}
 
 			sr.Close();
-			
+
 			nevKivalaszt.Text = "";
-			szerzoNeve.Text ="";
+			szerzoNeve.Text = "";
 			cim.Text = "";
 			Datum.Text = "";
 
 			listBox.Items.Refresh();
 
-			//listBox.ItemsSource = versKiir;
-			/*
-			string[] hetiVersek = File.ReadAllLines("eddigiversek.txt");
-			for (int i = 0; i < hetiVersek.Length; i++)
-			{
-				versAdatok.Add(hetiVersek[i]);
-			}
+			MessageBox.Show(versek.Count().ToString());
+			MessageBox.Show(versKiir.Count().ToString());
 
-			listBox.ItemsSource = versAdatok;*/
+
+			for (int i = 0; i < versek.Count; i++)
+			{
+                foreach(var j in listBox.Items)
+                {
+					j.Tag = versek[i].URL();
+
+				}
+            }
+
+
+
+		//listBox.ItemsSource = versKiir;
+		/*
+		string[] hetiVersek = File.ReadAllLines("eddigiversek.txt");
+		for (int i = 0; i < hetiVersek.Length; i++)
+		{
+			versAdatok.Add(hetiVersek[i]);
 		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            List<string> szures = new List<string>();
-            string nev = vizsgaltNev.Text;
-            string szerzo = vizsgaltSzerzo.Text;
-            string verscim = vizsgaltVerscim.Text;
+		listBox.ItemsSource = versAdatok;*/
+	}
 
-            
-			for (int i = 0; i< versek.Count; i++)
-			{
-				if (nev == versek[i].felelos || szerzo == versek[i].versSzerzo || verscim == versek[i].versCim)
-				{
-					szures.Add($"{versek[i].felelos} ; {versek[i].versSzerzo} - {versek[i].versCim} ; {versek[i].datum}");
-				}
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			List<string> szures = new List<string>();
+			string nev = vizsgaltNev.Text;
+			string szerzo = vizsgaltSzerzo.Text;
+			string verscim = vizsgaltVerscim.Text;
 
-                //MessageBox.Show(versek[i].ToString());
-            }
+			//MessageBox.Show(versek[versek.Count() - 1].felelos);
+
+			int adatSzam = 0;
+
 			
+
+			adatSzam = nev != "" ? adatSzam += 1 : adatSzam;
+			adatSzam = szerzo != "" ? adatSzam += 1 : adatSzam;
+			adatSzam = verscim != "" ? adatSzam += 1 : adatSzam;
+			
+			if (adatSzam == 3)
+			{
+				for (int i = 0; i < versek.Count; i++)
+				{
+					if (nev == versek[i].felelos && szerzo == versek[i].versSzerzo && verscim == versek[i].versCim)
+					{
+						szures.Add(versek[i].osszefuz());
+					}
+
+					//MessageBox.Show(versek[i].ToString());
+				}
+			}
+			else if(adatSzam == 2)
+			{
+				for (int i = 0; i < versek.Count; i++)
+				{
+					if ((nev == versek[i].felelos && szerzo == versek[i].versSzerzo) || 
+						(nev == versek[i].felelos && verscim == versek[i].versCim) ||
+						(verscim == versek[i].versCim && szerzo == versek[i].versSzerzo))
+					{
+						szures.Add(versek[i].osszefuz());
+					}
+
+					//MessageBox.Show(versek[i].ToString());
+				}
+			}
+			else
+			{
+				for (int i = 0; i < versek.Count; i++)
+				{
+					if (nev == versek[i].felelos || szerzo == versek[i].versSzerzo || verscim == versek[i].versCim)
+					{
+						szures.Add(versek[i].osszefuz());
+					}
+
+					//MessageBox.Show(versek[i].ToString());
+				}
+			}
+			
+
+
 			eredmeny.ItemsSource = szures;
 
+			listBox.Items.Refresh();
 
+		}
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+			if(listBox.SelectedItem is ListBoxItem SelectedItem)
+			{
+
+			}
         }
     }
 }
